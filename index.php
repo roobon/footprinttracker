bc08855910d1e419cdc1ae9e15dffdd2c1214deb
Syed Ziaul Habib <roobon@gmail.com>
2023-05-31 22:32:22 +0600

    Background added to login page

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#000000">
@@ -18,7 +17,18 @@
     <link rel="manifest" href="__manifest.json">
     <style>
         body {
-            background:url("assets/img/bg.png")
+            background: url("assets/img/bg_2.jpg");
+            color: aliceblue;
+
+        }
+
+
+        h1 {
+            color: aliceblue;
+        }
+
+        a:link {
+            color: aliceblue !important;
         }
     </style>
 </head>
@@ -33,19 +43,19 @@
 
 
     <!-- App Capsule -->
-    <div id="appCapsule" class="pt-0">
+    <div id="appCapsule" class="p-15">
 
         <div class="login-form mt-1">
-           
+
             <div class="section mt-1">
                 <h1>লগইন করুন</h1>
-                
+
             </div>
             <div class="section mt-1 mb-5">
                 <form action="app-pages.html">
-                    
-                <div class="form-check">
-  <!-- <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+
+                    <div class="form-check">
+                        <!-- <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
   <label class="form-check-label" for="exampleRadios1">
     ব্যক্তিগত
   </label>
@@ -62,76 +72,76 @@
     Disabled radio
   </label>
 </div> -->
-<div class="row" >
-    <div class="form-check form-check-inline">
-    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-    <label class="form-check-label" for="inlineRadio1">ব্যক্তিগত</label>
-    </div>
+                        <div class="row">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" checked name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                <label class="form-check-label" for="inlineRadio1">ব্যক্তিগত</label>
+                            </div>
 
-    <div class="form-check form-check-inline">
-    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-    <label class="form-check-label" for="inlineRadio2">বাসা/বাড়ি</label>
-    </div>
-</div>
-
-<div class="row">
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-  <label class="form-check-label" for="inlineRadio3">প্রতিষ্ঠান</label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-  <label class="form-check-label" for="inlineRadio3">প্রোডাক্ট</label>
-</div>
-</div>
-<div class="row">
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-  <label class="form-check-label" for="inlineRadio3">অন্যান্য</label>
-</div>
-</div>
-
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <input type="email" class="form-control" id="email1" placeholder="ইমেইল">
-                            <i class="clear-input">
-                                <ion-icon name="close-circle"></ion-icon>
-                            </i>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                <label class="form-check-label" for="inlineRadio2">বাসা/বাড়ি</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <input type="password" class="form-control" id="password1" placeholder="পাসওয়ার্ড">
-                            <i class="clear-input">
-                                <ion-icon name="close-circle"></ion-icon>
-                            </i>
+                        <div class="row">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+                                <label class="form-check-label" for="inlineRadio3">প্রতিষ্ঠান</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+                                <label class="form-check-label" for="inlineRadio3">প্রোডাক্ট</label>
+                            </div>
                         </div>
-                    </div>
+                        <div class="row">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+                                <label class="form-check-label" for="inlineRadio3">অন্যান্য</label>
+                            </div>
+                        </div>
 
-                    
+                        <div class="form-group boxed">
+                            <div class="input-wrapper">
+                                <input type="email" class="form-control" id="email1" placeholder="ইমেইল">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
 
-                    <div>
-                        <button type="submit" class="btn btn-success btn-block btn-lg">সাবমিট</button>
-                    </div>
-                    <div>
-                        <button type="submit" class="btn btn-primary btn-block btn-lg">গেস্ট লগইন</button>
-                    </div>
-                    <div>
-                        <a href="account_list.php" class="btn btn-primary btn-block btn-lg">নতুন একাউন্ট করুন</a>    
-                   
-                    </div>
-                    <div class="form-links mt-2">
+                        <div class="form-group boxed">
+                            <div class="input-wrapper">
+                                <input type="password" class="form-control" id="password1" placeholder="পাসওয়ার্ড">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+
+
+
                         <div>
-                            
+                            <button type="submit" class="btn btn-success btn-block btn-lg">সাবমিট</button>
                         </div>
-                        <div><a href="page-forgot-password.html" class="text-muted">পাসওয়ার্ড ভুলে গেছেন?</a></div>
-                    </div>
-                    <div class="section">
-                <img src="" alt="image" class="form-image">
-            </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary btn-block btn-lg">গেস্ট লগইন</button>
+                        </div>
+                        <div>
+                            <a href="account_list.php" class="btn btn-primary btn-block btn-lg">নতুন একাউন্ট করুন</a>
+
+                        </div>
+                        <div class="form-links mt-2">
+                            <div>
+
+                            </div>
+                            <div><a href="page-forgot-password.html" class="text-muted">পাসওয়ার্ড ভুলে গেছেন?</a></div>
+                        </div>
+
+
 
                 </form>
+                <img src="assets/img/partners.jpg" alt="image" width="100%">
             </div>
         </div>
 

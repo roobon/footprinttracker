a5a0659246e5d791408c6982d979df9c9bdce84b
Syed Ziaul Habib <roobon@gmail.com>
2023-05-31 22:05:39 +0600

    First commit

---
+++ account_list.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="theme-color" content="#000000">
+    <title>Carbon Foot Tracker</title>
+    <meta name="description" content="Mobilekit HTML Mobile UI Kit">
+    <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" sizes="32x32">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icon/192x192.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/customstyle.css">
+    <link rel="manifest" href="__manifest.json">
+    <style>
+        body {
+            background:url("assets/img/bg.png")
+        }
+    </style>
+</head>
+
+<body class="bg-white">
+
+    <!-- loader -->
+    <div id="loader">
+        <div class="spinner-border text-primary" role="status"></div>
+    </div>
+    <!-- * loader -->
+
+
+    <!-- App Capsule -->
+    <div id="appCapsule" class="pt-0">
+
+        <div class="login-form mt-1">
+           
+            <div class="section mt-1">
+                <h1>নতুন অ্যাকাউন্ট খুলুন</h1>
+                
+            </div>
+            <div class="section mt-1 mb-5">
+                <form action="app-pages.html">
+                    
+                <div class="form-check">       
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block btn-lg">ব্যক্তিগত</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block btn-lg">প্রতিষ্ঠান</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block btn-lg">বাসা/বাড়ি</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block btn-lg">প্রোডাক্ট</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block btn-lg">অন্যান্য</button>
+                    </div>
+                    
+                   
+
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- * App Capsule -->
+
+
+
+    <!-- ///////////// Js Files ////////////////////  -->
+    <!-- Jquery -->
+    <script src="assets/js/lib/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap-->
+    <script src="assets/js/lib/popper.min.js"></script>
+    <script src="assets/js/lib/bootstrap.min.js"></script>
+    <!-- Ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
+    <!-- Owl Carousel -->
+    <script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <!-- jQuery Circle Progress -->
+    <script src="assets/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
+    <!-- Base Js File -->
+    <script src="assets/js/base.js"></script>
+
+
+</body>
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="theme-color" content="#000000">
+    <title>Carbon Foot Tracker</title>
+    <meta name="description" content="Mobilekit HTML Mobile UI Kit">
+    <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" sizes="32x32">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icon/192x192.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/customstyle.css">
+    <link rel="manifest" href="__manifest.json">
+    <style>
+        body {
+            background:url("assets/img/bg.png")
+        }
+    </style>
+</head>
+
+<body class="bg-white">
+
+    <!-- loader -->
+    <div id="loader">
+        <div class="spinner-border text-primary" role="status"></div>
+    </div>
+    <!-- * loader -->
+
+
+    <!-- App Capsule -->
+    <div id="appCapsule" class="pt-0">
+
+        <div class="login-form mt-1">
+           
+            <div class="section mt-1">
+                <h1>লগইন করুন</h1>
+                
+            </div>
+            <div class="section mt-1 mb-5">
+                <form action="app-pages.html">
+                    
+                <div class="form-check">
+  <!-- <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+  <label class="form-check-label" for="exampleRadios1">
+    ব্যক্তিগত
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+  <label class="form-check-label" for="exampleRadios2">
+    Second default radio
+  </label>
+</div>
+<div class="form-check disabled">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled>
+  <label class="form-check-label" for="exampleRadios3">
+    Disabled radio
+  </label>
+</div> -->
+<div class="row" >
+    <div class="form-check form-check-inline">
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+    <label class="form-check-label" for="inlineRadio1">ব্যক্তিগত</label>
+    </div>
+
+    <div class="form-check form-check-inline">
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+    <label class="form-check-label" for="inlineRadio2">বাসা/বাড়ি</label>
+    </div>
+</div>
+
+<div class="row">
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+  <label class="form-check-label" for="inlineRadio3">প্রতিষ্ঠান</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+  <label class="form-check-label" for="inlineRadio3">প্রোডাক্ট</label>
+</div>
+</div>
+<div class="row">
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+  <label class="form-check-label" for="inlineRadio3">অন্যান্য</label>
+</div>
+</div>
+
+                    <div class="form-group boxed">
+                        <div class="input-wrapper">
+                            <input type="email" class="form-control" id="email1" placeholder="ইমেইল">
+                            <i class="clear-input">
+                                <ion-icon name="close-circle"></ion-icon>
+                            </i>
+                        </div>
+                    </div>
+
+                    <div class="form-group boxed">
+                        <div class="input-wrapper">
+                            <input type="password" class="form-control" id="password1" placeholder="পাসওয়ার্ড">
+                            <i class="clear-input">
+                                <ion-icon name="close-circle"></ion-icon>
+                            </i>
+                        </div>
+                    </div>
+
+                    
+
+                    <div>
+                        <button type="submit" class="btn btn-success btn-block btn-lg">সাবমিট</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary btn-block btn-lg">গেস্ট লগইন</button>
+                    </div>
+                    <div>
+                        <a href="account_list.php" class="btn btn-primary btn-block btn-lg">নতুন একাউন্ট করুন</a>    
+                   
+                    </div>
+                    <div class="form-links mt-2">
+                        <div>
+                            
+                        </div>
+                        <div><a href="page-forgot-password.html" class="text-muted">পাসওয়ার্ড ভুলে গেছেন?</a></div>
+                    </div>
+                    <div class="section">
+                <img src="" alt="image" class="form-image">
+            </div>
+
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- * App Capsule -->
+
+
+
+    <!-- ///////////// Js Files ////////////////////  -->
+    <!-- Jquery -->
+    <script src="assets/js/lib/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap-->
+    <script src="assets/js/lib/popper.min.js"></script>
+    <script src="assets/js/lib/bootstrap.min.js"></script>
+    <!-- Ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
+    <!-- Owl Carousel -->
+    <script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <!-- jQuery Circle Progress -->
+    <script src="assets/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
+    <!-- Base Js File -->
+    <script src="assets/js/base.js"></script>
+
+
+</body>
+
+</html>
